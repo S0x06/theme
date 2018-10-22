4d9040d463f018e1f00d254cacf600b3650b5047
S0x06 <735594423@qq.com>
2018-10-22 18:15:25 +0800

    jwt

---
+++ service/middleware/jwt/jwt.go
@@ -0,0 +1,22 @@
+package jwt
+
+
+func GenerateToken(){
+
+}
+
+
+
+func ParseToken(token string) (*Claims, error) {
+	tokenClaims, err := jwt.ParseWithClaims(token, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if tokenClaims != nil {
+		if claims, ok := tokenClaims.Claims.(*Claims); ok && tokenClaims.Valid {
+			return claims, nil
+		}
+	}
+
+	return nil, err
+}
